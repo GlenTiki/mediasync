953b907eefc6666a69859b7b78f5a025d0ff897a
Glen Keane <glenkeane.94@gmail.com>
2016-01-26 00:12:35 +0000

    testing VCAP_SERVICES

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ var server = require('./server/server.js')
 
 server.create(() => console.log('now running'))
 
-console.log(process.env)
+if (process.env.VCAP_SERVICES) {
+  console.log(process.env.VCEP_SERVICES)
+  var VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES)
+  console.log(VCAP_SERVICES)
+  console.log(JSON.stringify(VCAP_SERVICES, null, 2))
+}
